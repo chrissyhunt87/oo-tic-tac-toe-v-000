7feb63336ceb1847c33738fb16b6f9b632741c7d
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-08 23:19:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,4 +35,8 @@ class TicTacToe
     @board[location] != " " && @board[location] != ""
   end
 
+  def valid_move?(@board, index)
+    index.between?(0,8) && !position_taken?(@board, index)
+  end
+
 end
